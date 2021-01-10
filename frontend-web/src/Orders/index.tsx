94afba43f49f70ff8fd603c9255aec34e9f7762e
Klismann Barros <klismannbarros@eng.ci.ufpb.br>
2021-01-10 09:12:15 -0300

    Part 1 front-web done

--- frontend-web/src/Orders/index.tsx
+++ frontend-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
 import './styles.css';
-import './StepsHeader';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import './styles.css';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from './api';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts().then(response => setProducts(response.data)).catch(error => alert(error));
+    }, [])
+
+    useEffect(()=>{
+        
+    }, []);
+
      return (  
         <div className="orders-container">
             <StepsHeader></StepsHeader>
+            <ProductsList products={products}></ProductsList>
         </div>
      )
 }
